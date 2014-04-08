86cdbd573bb38d862dfa9082d5018ba83ad58661
Jare Guo <jare85@163.com>
2014-04-09 03:00:27 +0800

    stash

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ require('server/index');
 
 global.$ = $;
 global.SERVER_URL = 'http://localhost:8888/';
-var FILE_COUNT = 1000;
+
+var FILE_COUNT = 10;
 var BYTES_TO_READ = 512;
 
 if (process.platform == 'win32') {
-    var randBigDir = 'D:\\';
+    var randBigDir = 'D:\\K\\';
+    //var randBigDir = 'D:\\K\\Dev\\node.js\\FileSystemBenchmark\\node_modules\\server';
 }
 else {
     var randBigDir = '/';
@@ -22,7 +24,6 @@ var defaultPath = process.cwd();
 var events = require('events');
 var path = require('path');
 var fs = require('fs');
-require('buffer');
 
 if (process.platform == 'win32') {
     var win32 = require("win32");
@@ -30,7 +31,25 @@ if (process.platform == 'win32') {
 
 // local variables
 
-var curFileCount = 0;
+var curFileCount = 0;      // 剩余文件数
+var curConcurrency = 0;    // 当前并发数
+var maxConcurrency = 0;    // 最大并发数
+
+function startTest(testName) {
+    curFileCount = FILE_COUNT;
+    console.log('测试中...');
+    curConcurrency = 0;
+    maxConcurrency = 1;
+
+    console.time(testName);
+    return {
+        end : function () {
+            console.timeEnd(testName);
+            console.assert(curConcurrency == 0, 'curConcurrency: ' + curConcurrency);
+            console.log('最终读取数量: %d 最大并发数: %d', FILE_COUNT - curFileCount, maxConcurrency);
+        }
+    };
+}
 
 // main
 
@@ -42,20 +61,20 @@ $(document).ready(function () {
     // register events
 
     $('#Test1').click(function () {
-        curFileCount = FILE_COUNT;
         console.log('测试目录: %s\n读取数量: %d\n数据大小: %d Byte', randBigDir, curFileCount, BYTES_TO_READ);
 
-        var testName = '本地随机读取测试(同步, 无并发)';
-        //content.html(testName);
-        console.time(testName);
+        var test = startTest('本地随机读取测试(同步, 无并发)');
         RandReadTest_NativeSync(randBigDir);
-        console.timeEnd(testName);
+        test.end();
 
-        curFileCount = FILE_COUNT;
-        testName = 'http随机读取测试(同步, 无并发)';
-        console.time(testName);
+        test = startTest('http随机读取测试(同步, 无并发)');
         RandReadTest_HttpSync(randBigDir, function () {
-            console.timeEnd(testName);
+            test.end();
+
+            test = startTest('本地随机读取测试(异步, 并发)');
+            RandReadTest_Native(randBigDir, function () {
+                test.end();
+            });
         });
     });
     $('#Test2').click(function () {
@@ -67,10 +86,10 @@ $(document).ready(function () {
 });
 
 function RandReadTest_NativeSync(dir) {
-    var files = fs.readdirSync(dir);
-    //console.log(dir + files.length);
-    for (var i = 0; i < files.length && curFileCount > 0; ++i) {
-        var subPath = path.join(dir, files[i]);
+    var items = fs.readdirSync(dir);
+    //console.log(dir + items.length);
+    for (var i = 0; i < items.length && curFileCount > 0; ++i) {
+        var subPath = path.join(dir, items[i]);
         //console.log(subPath);
         try {
             var stat = fs.statSync(subPath);
@@ -92,6 +111,7 @@ function RandReadTest_NativeSync(dir) {
             }
             //try {
                 var buffer = new Buffer(BYTES_TO_READ);
+                buffer.fill(0);
                 fs.readSync(fd, buffer, 0, BYTES_TO_READ);    
             //}
             //finally {
@@ -101,6 +121,94 @@ function RandReadTest_NativeSync(dir) {
     }
 }
 
+function AddConcurrency(num) {
+    curConcurrency += num;
+    if (curConcurrency > maxConcurrency) {
+        maxConcurrency = curConcurrency;
+    }
+}
+
+function statFiles(dir, items, callback) {
+    var pending = items.length;
+    for (var i = 0; i < items.length; ++i) {
+        var subPath = path.join(dir, items[i]);
+        //AddConcurrency(1);
+        fs.stat(subPath, function functor(i2) {
+            return function (err, stat) {
+                //AddConcurrency(-1);
+                items[i2] = { name : items[i2] };
+                if (err) {
+                    items[i2].type = "undefined";
+                }
+                else {
+                    items[i2].type = (stat.isDirectory() ? "folder" : "file");
+                }
+                --pending;
+                //console.log(pending);
+                if (pending == 0) {
+                    callback();
+                }
+            };
+        }(i));
+    }
+}
+
+function RandReadTest_Native(dir, callback) {
+    AddConcurrency(1);
+    fs.readdir(dir, function (err, items) {
+        AddConcurrency(-1);
+        if (err) {
+            callback();
+            return;
+        }
+        statFiles(dir, items, function () {
+            //console.log(dir);
+            var i = 0;
+            var nextLoop = function () {
+                console.log(curFileCount);
+                if (i < items.length && curFileCount > 0) {
+                    var subPath = path.join(dir, items[i].name);
+                    var type = items[i].type;
+                    ++i;
+                    if (type == "undefined") {
+                        --curFileCount;
+                        nextLoop();
+                        return;
+                    }
+                    else if (type == "folder") {
+                        RandReadTest_Native(subPath, nextLoop);
+                    }
+                    else {
+                        AddConcurrency(1);
+                        fs.open(subPath, "rs", 0666, function (err, fd) {
+                            if (err) {
+                                console.log(err);
+                                AddConcurrency(-1);
+                                --curFileCount; // curFileCount应该是访问前就减掉，回掉时判断是否还有在并发
+                                return;
+                            }
+                            var buffer = new Buffer(BYTES_TO_READ);
+                            buffer.fill(0);
+                            fs.read(fd, buffer, 0, BYTES_TO_READ, null, function (err, bytesRead, buffer) {
+                                console.assert(!err);
+                                fs.close(fd, function () {
+                                    --curFileCount;
+                                    AddConcurrency(-1);
+                                });
+                            });
+                        });
+                        nextLoop(); // we dont wait for async finished
+                    }
+                }
+                else {
+                    callback();
+                }
+            }
+            nextLoop();
+        });
+    });
+}
+
 // 客户端和服务端是异步通讯，但服务端采用同步方法，并发数只有一个
 function RandReadTest_HttpSync(dir, callback) {
     //console.warn('get ' + dir);
@@ -108,7 +216,7 @@ function RandReadTest_HttpSync(dir, callback) {
         .done(function (data) {
             var items = data.items;
             var i = 0;
-            var nextLoop = function (callNextLoop) {
+            var nextLoop = function () {
                 if (i < items.length && curFileCount > 0) {
                     var subPath = path.join(dir, items[i].name);
                     //console.log(subPath);
@@ -120,7 +228,8 @@ function RandReadTest_HttpSync(dir, callback) {
                     }
                     else if (type == 'file') {
                         $.get(global.SERVER_URL + "open_sync", { path : subPath, length : BYTES_TO_READ })
-                            .done(function () {
+                            .done(function (data) {
+                                //console.log(data);
                                 --curFileCount;
                                 nextLoop();
                             })
